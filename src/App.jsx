0b13b97f99971f622adb3e61920d137ca813bc6f
Kamani kumari <kaminigupta0801@gmail.com>
2024-01-12 16:07:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './Components/Navbar'
+import Slider from './Components/Slider'
+
+function App() {
+  
+
+  return (
+    <>
+     <Navbar/>
+     <Slider/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+  <div>
+    <div ClassName = 'max-h-40 overflow-hidden'>
+      <Slider/>
+    </div>
+  </div>
+  )
+}
+
+export default Home
